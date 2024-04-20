38df48d8bc3b14ae4bd65f3cdeb2b00ebe79bb55
kaedev <85381067+akaedev@users.noreply.github.com>
2024-04-20 14:48:20 +0500

    поставлен чатбот еа generator app py

--- backend/generator_app.py
+++ backend/generator_app.py
@@ -1,27 +1,8 @@
-from dotenv import load_dotenv
-import streamlit as st
-import os
-import google.generativeai as ggi
+import google.generativeai as genai
 
-load_dotenv(".env")
+genai.configure(api_key='AIzaSyCqfehAC7iwbfBc8jnMRgSt8OIa2Z02kpo')
 
-fetcheed_api_key = os.getenv("API_KEY")
-ggi.configure(api_key = fetcheed_api_key)
+model = genai.GenerativeModel('gemini-pro')
+response = model.generate_content('Напиши мне туристический маршрут для путешествий по Астане')
 
-model = ggi.GenerativeModel("gemini-pro")
-chat = model.start_chat()
-
-def LLM_Response(question):
-    response = chat.send_message(question,stream=True)
-    return response
-
-st.title("Chat Application using Gemini Pro")
-
-user_quest = st.text_input("Ask a question:")
-btn = st.button("Ask")
-
-if btn and user_quest:
-    result = LLM_Response(user_quest)
-    st.subheader("Response : ")
-    for word in result:
-        st.text(word.text)
+print(response.text)
