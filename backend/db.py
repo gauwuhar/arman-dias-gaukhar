78fc0384d7f2d474c973b080d95124b732971142
kaedev <85381067+akaedev@users.noreply.github.com>
2024-04-20 21:51:57 +0500

    полный рефактор всего

--- backend/db.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient('mongodb://localhost:27017/')
-db = client.chatbot  # 'chatbot' is the name of the database
-
-def save_message(user_id, message):
-    messages = db.messages  # 'messages' is a collection
-    messages.insert_one({"user_id": user_id, "message": message})
